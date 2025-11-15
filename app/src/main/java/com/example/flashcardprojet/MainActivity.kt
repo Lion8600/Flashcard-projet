e6779590fe5394448bd5e53e49d448a137737067
unknown <lionel12@gmail.com>
2025-11-15 18:38:42 -0500

    adding listener onflashcardquestion

--- app/src/main/java/com/example/flashcardprojet/MainActivity.kt
+++ app/src/main/java/com/example/flashcardprojet/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.example.flashcardprojet
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val flashcardquestion = findViewById<TextView>(R.id.flashcard_question)
+        val flashcardreponse = findViewById<TextView>(R.id.flashcard_reponse)
+
+        flashcardquestion.setOnClickListener{
+            flashcardquestion.visibility= View.INVISIBLE
+            flashcardreponse.visibility= View.VISIBLE
         }
     }
 }
